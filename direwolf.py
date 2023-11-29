d5fcb289ae5dba51b0a2f13528a68a57f1f61959
KKH <info.konham@gmail.com>
2023-11-29 09:56:51 -0500

    Write and pass first three direwolf tests, fail fourth

---
+++ direwolf.py
@@ -0,0 +1,5 @@
+class Direwolf:
+    def __init__(self, name = str, home = str, size = str):
+        self.name = name
+        self.home = 'Beyond the Wall' else home
+        self.size = 'Massive'
