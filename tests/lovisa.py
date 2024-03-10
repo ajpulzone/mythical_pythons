f53ce28148891e9d74f6b924c097cd80b5f76ec5
Annie Pulzone <ajpulzone@gmail.com>
2024-03-10 09:31:52 -0700

    Create is_brilliant function

---
+++ tests/lovisa.py
@@ -0,0 +1,9 @@
+class Lovisa:
+  def __init__(self, title = str, characteristics = "brilliant"):
+    self.title = title 
+    self.characteristics = []
+
+  def is_brilliant(self):
+    return self.characteristics == "brilliant"
+  
+
