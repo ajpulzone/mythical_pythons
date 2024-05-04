300423783d00fe8bb62899b0e2383355bd11299f
Annie Pulzone <ajpulzone@gmail.com>
2024-05-04 09:13:38 -0700

    In progress, place holder commit while getting tests to pass

---
+++ tests/ogre.py
@@ -0,0 +1,15 @@
+class Ogre:
+  def __init__(self, name = str, home = "Swamp"):
+    self.name = name
+    self.home = home
+    self.encounters = 0
+    self.swings = 0
+
+  def encounter(self, human):
+    # human.encounter_counter += 1
+    if human.notices_ogre():
+      self.swing_at(human)
+
+  def swing_at(self, human):
+    if human.notices_ogre():
+      self.swings += 1
